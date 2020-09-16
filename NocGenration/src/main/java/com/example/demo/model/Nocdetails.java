42f07feb455afc401e475ff658b8ed6e839cf163
pritijange <71375261+pritijange@users.noreply.github.com>
2020-09-16 15:54:21 +0530

    Update Nocdetails.java
    by git

--- NocGenration/src/main/java/com/example/demo/model/Nocdetails.java
+++ NocGenration/src/main/java/com/example/demo/model/Nocdetails.java
@@ -24,7 +24,7 @@ public class Nocdetails
 {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	// @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int nocId;
 		
 	@Temporal(TemporalType.TIMESTAMP)
